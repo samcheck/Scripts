cc5c8ad22d2d96e25c2a6ca362d801c9f64ab843
samcheck <samcheckster@gmail.com>
2016-10-16 13:39:58 -0500

    Adding docstring

--- hashSHA1.py
+++ hashSHA1.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 # hashSHA1.py - hash SHA-1 of a given file and print
-# Usage: hashSHA1.py <file>
+# Usage:import hashSHA1
+#       hashSHA1.hashSHA1(<file>) - returns SHA1 hash of file
 
 import hashlib, os
 
 BLOCKSIZE = 65536 # set BLOCKSIZE for large files
 
 def hashSHA1(in_file):
-    if os.path.isfile(in_file):
+    ''' hashSHA1(<input file>) - takes a file (or path to file)
+        returns the hex digest of the input'''
+    if os.path.isfile(in_file): # Check if input is a file and hash
         sha1 =  hashlib.sha1()
         with open(in_file, 'rb') as file_to_hash:
             hash_buffer = file_to_hash.read(BLOCKSIZE)
