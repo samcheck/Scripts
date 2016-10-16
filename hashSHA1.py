3023deacb4bf151865d7cb5871beeea273480a4a
samcheck <samcheckster@gmail.com>
2016-10-16 13:16:07 -0500

    Change hashSHA1 to function

--- hashSHA1.py
+++ hashSHA1.py
@@ -2,15 +2,16 @@
 # hashSHA1.py - hash SHA-1 of a given file and print
 # Usage: hashSHA1.py <file>
 
-import hashlib, sys
+import hashlib, os
 
 BLOCKSIZE = 65536 # set BLOCKSIZE for large files
 
 def hashSHA1(in_file):
-    sha1 =  hashlib.sha1()
-    with open(sys.argv[1], 'rb') as file_to_hash:
-        hash_buffer = file_to_hash.read(BLOCKSIZE)
-        while len(hash_buffer) > 0:
-            sha1.update(hash_buffer)
+    if os.path.isfile(in_file):
+        sha1 =  hashlib.sha1()
+        with open(in_file, 'rb') as file_to_hash:
             hash_buffer = file_to_hash.read(BLOCKSIZE)
-    return(sha1.hexdigest())
+            while len(hash_buffer) > 0:
+                sha1.update(hash_buffer)
+                hash_buffer = file_to_hash.read(BLOCKSIZE)
+        return(sha1.hexdigest())
