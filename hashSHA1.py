23e2cba211126f1980d9dc0a5302f719e3e4ab78
samcheck <samcheckster@gmail.com>
2016-10-16 12:24:53 -0500

    sha1 hasher function

---
+++ hashSHA1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# hashSHA1.py - hash SHA-1 of a given file and print
+# Usage: hashSHA1.py <file>
+
+import hashlib, sys
+
+BLOCKSIZE = 65536 # set BLOCKSIZE for large files
+
+def hashSHA1(in_file):
+    sha1 =  hashlib.sha1()
+    with open(sys.argv[1], 'rb') as file_to_hash:
+        hash_buffer = file_to_hash.read(BLOCKSIZE)
+        while len(hash_buffer) > 0:
+            sha1.update(hash_buffer)
+            hash_buffer = file_to_hash.read(BLOCKSIZE)
+    return(sha1.hexdigest())
