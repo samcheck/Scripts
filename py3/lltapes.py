328d2efac0a9b1407771691ecdb7e952ae0a8cfa
samcheck <samcheckster@gmail.com>
2016-12-06 19:21:49 -0600

    option to download mp3s

--- py3/lltapes.py
+++ py3/lltapes.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import sys
+import os
 
 
 show_id = sys.argv[1]
 URL_BASE = "http://www.lovelinetapes.com/shows/?id="
 MP3_BASE = "http://recordings.lovelinetapes.com/"
 url = (URL_BASE + show_id)
+exten = '.mp3'
 print('Downloading page %s...' % url)
 res = requests.get(url)
 res.raise_for_status()
@@ -21,6 +23,7 @@ right_link = soup.find_all('a', attrs={'style': 'float: right;', 'class': 'showL
 title = soup.find('title')
 page_url = soup.find('meta', attrs={'property': 'og:url'}) #full link w/ redirect to mp3 is in this meta tag
 mp3_link = re.search(r'h\=\w+', page_url['content']).group().replace('h=', '') #match on the mp3 link format
+url_ep = MP3_BASE + mp3_link + exten
 title_date = re.search(r'\d?\d\/\d?\d\/\d{4}', title.text)
 title_guest = re.search(r'\d{4}.*', title.text)
 
@@ -33,13 +36,26 @@ else:
             print('Guest: ' + re.sub(r'\d{4} - ', '', title_guest.group()))
         if title_date is not None:
             print('Date: ' + title_date.group())
-        print(MP3_BASE + mp3_link + '.mp3')
+        print(url_ep)
         print('\nPrevious Show:')
         print(left_link[0].get('href'))
         print(left_link[0].find('div', attrs={'class': 'details'}).text.strip())
         print('\nNext Show:')
         print(right_link[0].get('href'))
         print(right_link[0].find('div', attrs={'class': 'details'}).text.strip())
+        save_name = ("Loveline - " + title_date.group().replace('/', '-') +
+                    re.sub(r'\d{4}', '', title_guest.group()) + exten)
+        if len(sys.argv) == 3:
+            if sys.argv[2] == 'd':
+                curpath = os.path.abspath(os.curdir)
+                # Download the episode
+                print('Downloading %s...' % url_ep)
+                r = requests.get(url_ep, stream=True)
+                if r.status_code == 200:
+                    print('Saving as %s' % save_name)
+                    with open(os.path.join(curpath, save_name), 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=1024):
+                            f.write(chunk)
     except requests.exceptions.MissingSchema:
         # skip this
         print('Could not find link')
