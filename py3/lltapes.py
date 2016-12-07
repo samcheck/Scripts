e60181414ebc8b62cc1de2210cd6d8a1d7218870
samcheck <samcheckster@gmail.com>
2016-12-07 17:32:03 -0600

    refactor ep save function

--- py3/lltapes.py
+++ py3/lltapes.py
@@ -27,6 +27,34 @@ url_ep = MP3_BASE + mp3_link + exten
 title_date = re.search(r'\d?\d\/\d?\d\/\d{4}', title.text)
 title_guest = re.search(r'\d{4}.*', title.text)
 
+def save_ep(show_id):
+    url = (URL_BASE + show_id)
+    exten = '.mp3'
+    print('Downloading page %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = BeautifulSoup(res.text, "lxml")
+    title = soup.find('title')
+    title_date = re.search(r'\d?\d\/\d?\d\/\d{4}', title.text)
+    title_guest = re.search(r'\d{4}.*', title.text)
+    mp3_link = re.search(r'h\=\w+', page_url['content']).group().replace('h=', '') #match on the mp3 link format
+    url_ep = MP3_BASE + mp3_link + exten
+
+    save_name = ("Loveline - " + title_date.group().replace('/', '-') +
+                re.sub(r'\d{4}', '', title_guest.group()) + exten)
+    curpath = os.path.abspath(os.curdir)
+    # Download the episode
+    print('Downloading %s...' % url_ep)
+    r = requests.get(url_ep, stream=True)
+    r.raise_for_status()
+    if r.status_code == 200:
+        print('Saving as %s' % save_name)
+        with open(os.path.join(curpath, save_name), 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                f.write(chunk)
+
+
 if left_link == []:
     print('Could not find link')
 else:
@@ -43,19 +71,9 @@ else:
         print('\nNext Show:')
         print(right_link[0].get('href'))
         print(right_link[0].find('div', attrs={'class': 'details'}).text.strip())
-        save_name = ("Loveline - " + title_date.group().replace('/', '-') +
-                    re.sub(r'\d{4}', '', title_guest.group()) + exten)
         if len(sys.argv) == 3:
             if sys.argv[2] == 'd':
-                curpath = os.path.abspath(os.curdir)
-                # Download the episode
-                print('Downloading %s...' % url_ep)
-                r = requests.get(url_ep, stream=True)
-                if r.status_code == 200:
-                    print('Saving as %s' % save_name)
-                    with open(os.path.join(curpath, save_name), 'wb') as f:
-                        for chunk in r.iter_content(chunk_size=1024):
-                            f.write(chunk)
+                save_ep(show_id)
     except requests.exceptions.MissingSchema:
         # skip this
         print('Could not find link')
