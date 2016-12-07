e825775bb88cbe2317199691a17aac240c18d637
samcheck <samcheckster@gmail.com>
2016-12-06 18:28:51 -0600

    use regex to get mp3 link, date and guest info

--- py3/lltapes.py
+++ py3/lltapes.py
@@ -19,18 +19,25 @@ soup = BeautifulSoup(res.text, "lxml")
 left_link = soup.find_all('a', attrs={'style': 'float: left;', 'class': 'showLink'})
 right_link = soup.find_all('a', attrs={'style': 'float: right;', 'class': 'showLink'})
 title = soup.find('title')
-mp3 = soup.find('meta', attrs={'property': 'og:url'})
+page_url = soup.find('meta', attrs={'property': 'og:url'}) #full link w/ redirect to mp3 is in this meta tag
+mp3_link = re.search(r'h\=\w+', page_url['content']).group().replace('h=', '') #match on the mp3 link format
+title_date = re.search(r'\d?\d\/\d?\d\/\d{4}', title.text)
+title_guest = re.search(r'\d{4}.*', title.text)
 
 if left_link == []:
     print('Could not find link')
 else:
     try:
         print(title.text)
-        print(mp3['content'])
-        print('Previous Show:')
+        if title_guest is not None:
+            print('Guest: ' + re.sub(r'\d{4} - ', '', title_guest.group()))
+        if title_date is not None:
+            print('Date: ' + title_date.group())
+        print(MP3_BASE + mp3_link + '.mp3')
+        print('\nPrevious Show:')
         print(left_link[0].get('href'))
         print(left_link[0].find('div', attrs={'class': 'details'}).text.strip())
-        print('Next Show:')
+        print('\nNext Show:')
         print(right_link[0].get('href'))
         print(right_link[0].find('div', attrs={'class': 'details'}).text.strip())
     except requests.exceptions.MissingSchema:
