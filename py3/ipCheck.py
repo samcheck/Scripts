10f492ff723e42b193edf15076587acd056dbaa2
samcheck <samcheckster@gmail.com>
2016-12-10 16:12:18 -0600

    update with new api

--- py3/ipCheck.py
+++ py3/ipCheck.py
@@ -3,13 +3,12 @@
 
 import json, requests
 
-# Download the JSON data from ip-api.com's API
-url ='http://ip-api.com/json'
+# Download the JSON data from http://freegeoip.net/json/
+url ='http://freegeoip.net/json/'
 response = requests.get(url)
 response.raise_for_status()
 
 # Load JSON data into a Python variable and print
 ipData = json.loads(response.text)
-print('Current IP:', ipData['query'])
-print('Current IP location:', ipData['city'], ipData['country'])
-print('Current ISP:', ipData['org'])
+print('Current IP:', ipData['ip'])
+print('Current IP location:', ipData['city'], ipData['country_name'])
